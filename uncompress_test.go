adfcf06af84f761103b4f52fa6d70ff2373d8cd9
Justin Chadwell <me@jedevc.com>
2022-04-07 15:24:00 +0100

    Fix non-continuous link groups
    The RPM payloadReader attempts to gracefully detect and handle hard
    links - however, it was relying on the actual data appearing directly
    after a series of hard links to that data. In practice, however, not all
    RPMs follow this format, and the actual data may appear far after when
    it is defined.
    
    This patch fixes this behaviour by correctly tracking all inodes seen so
    far, similarly to how the Extract() method already works today.
    Additionally, a test is added to ensure this doesn't regress.
    
    Co-Authored-By: Ilya Dmitrichenko <errordeveloper@gmail.com>

--- uncompress_test.go
+++ uncompress_test.go
@@ -1,6 +1,8 @@
 package rpmutils
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"io"
 	"io/ioutil"
 	"os"
@@ -54,4 +56,40 @@ func TestUncompressEmpty(t *testing.T) {
 	require.NoError(t, err)
 	_, err = payload.Next()
 	assert.ErrorIs(t, err, io.EOF)
+}
+
+func TestUncompressNonContinuousLinkGroups(t *testing.T) {
+	f, err := os.Open("testdata/crypto-policies-20210617-1.gitc776d3e.el8.noarch.rpm")
+	require.NoError(t, err)
+	defer f.Close()
+	rpm, err := ReadRpm(f)
+	require.NoError(t, err)
+	digestAlgo, err := rpm.Header.GetInts(FILEDIGESTALGO)
+	require.NoError(t, err)
+	require.Equal(t, []int{PGPHASHALGO_SHA256}, digestAlgo)
+
+	payload, err := rpm.PayloadReaderExtended()
+	require.NoError(t, err)
+
+	for {
+		file, err := payload.Next()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+
+		data, err := io.ReadAll(payload)
+		require.NoError(t, err)
+
+		if payload.IsLink() {
+			require.Equal(t, 0, len(data), "wrong size read for linked file %q", file.Name())
+			continue
+		}
+
+		require.Equal(t, int(file.Size()), len(data), "wrong size read for regular file %q", file.Name())
+		if file.Size() > 0 {
+			checksum := sha256.Sum256(data)
+			require.Equal(t, file.Digest(), hex.EncodeToString(checksum[:]), "checksums did not match for file %q", file.Name())
+		}
+	}
 }
